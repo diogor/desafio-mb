2c9c9440af0fd91719edd7206d469f604c5faa36
Diogo Lima <diogooo@gmail.com>
2024-10-22 19:30:17 -0300

    add response info for 400

--- app/web/main.py
+++ app/web/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 
 from app.core.services import get_symbol_price
 from app.web.request import CoinRequest
@@ -7,11 +7,17 @@ from app.web.response import CoinResponse
 app = FastAPI()
 
 
-@app.post("/coin_infos", response_model=CoinResponse)
+@app.post(
+    "/coin_infos",
+    response_model=CoinResponse,
+    responses={400: {"description": "Unknown symbol"}},
+)
 async def coin_infos(coin_request: CoinRequest) -> CoinResponse:
     response = get_symbol_price(coin_request.symbol)
 
     if not response:
-        raise HTTPException(status_code=404, detail="Coin not found")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Unknown symbol"
+        )
 
     return response
